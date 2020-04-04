f3555f3c5493e06f8a5ba2549bb31d6aea8ad2bf
AvKirill <kirillavdonin2014@yandex.ru>
2020-04-04 20:06:20 +0300

    добавил класс HttpClient с HTTP запросами

---
+++ app/src/main/java/com/example/todoapp/HttpClient.java
@@ -0,0 +1,26 @@
+package com.example.todoapp;
+
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+public class HttpClient {
+
+    private static final String BASE_URL = "https://26.183.45.79:8080/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+
+}
